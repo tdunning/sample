8614e024fd8fb64c80d557b4a88f7b3a3f259091
Ted Dunning <ted.dunning@gmail.com>
2023-03-13 09:39:21 -0700

    Added generation scripts, removed use of log-synth

--- synth/server_inventory.s
+++
@@ -1,7 +0,0 @@
-[
-    {"name":"serialnum", "class":"int", "min":0, "max":1000000, "skew":0},
-    {"name": "server_type", "class":"int", "min":1, "max":19},
-    {"name": "location", "class":"string", "dist":{"ND-01":5, "ND-02":3, "WA-01": 2, "DE-1": 2}}
-]
-
-    
